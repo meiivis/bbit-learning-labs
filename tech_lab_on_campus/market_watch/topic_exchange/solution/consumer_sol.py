5d29c481f5f2f0acc128cf9ba020337c5f89f744
danapantoja <danapantoja145@gmail.com>
2026-04-17 15:03:35 -0400

    implement

---
+++ tech_lab_on_campus/market_watch/topic_exchange/solution/consumer_sol.py
@@ -0,0 +1,54 @@
+
+from consumer_interface import mqConsumerInterface
+import os
+import pika
+import sys
+class mqConsumer(mqConsumerInterface):
+    def __init__(
+        self, binding_key: str, exchange_name: str, queue_name: str
+    ) -> None:
+        self.binding_key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+
+        self.setupRMQConnection()
+    def setupRMQConnection(self) -> None:
+
+        #connections
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.connection = pika.BlockingConnection(parameters=conParams)
+        self.channel = self.connection.channel()
+        #create queue
+        self.channel.queue_declare(queue=self.queue_name, exchange_type='topic')
+        routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+        
+        #create exchange
+        self.exchange = self.channel.exchange_declare(exchange=self.exchange_name)
+        #bind queue
+        self.channel.queue_bind(
+            queue= self.queue_name,
+            routing_key= self.binding_key,
+            exchange=self.exchange_name,
+        )
+        self.channel.basic_consume(
+            self.queue_name, self.on_message_callback, auto_ack=False
+        )
+
+    #done
+    def on_message_callback(
+        self, channel, method_frame, header_frame, body
+    ) -> None:
+        
+        self.channel.basic_ack(method_frame.delivery_tag, False)
+        print(body)
+        
+    
+
+    #done
+    def startConsuming(self) -> None:
+        print('[*] Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
+
+
+
+
